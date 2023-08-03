bc1b8eafdf5b34bf2bc74d6137c6d940cb5bcdac
Teena Kunwar Chouhan <teenakchouhan@gmail.com>
2023-08-03 13:46:01 +0530

    commit

---
+++ Test Suites/NewTestSuite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>NewTestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>7</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>e6e3d123-1991-47e2-80ff-cde0b3bbc4d7</testSuiteGuid>
+</TestSuiteEntity>
